1c5c4c0487b3417a366812929880b88793c95e88
DanielKelley117 <117874058+DanielKelley117@users.noreply.github.com>
2022-12-21 20:34:51 -0500

    Update Battleship.java

---
+++ Battleship.java
@@ -0,0 +1,12 @@
+//My name is Daniel Kelley and this is part of my final project for CS110. This contains the data for a BattleShip object, which is a Ship.
+public class Battleship extends Ship
+{
+   /*This is the BattleShip constructor.
+      It takes in no arguments, and just calls the super constructor with a size of 4.
+   */
+   public Battleship()
+   {
+      super(4);
+   }
+   
+}
