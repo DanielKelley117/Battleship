c09cc69f7ea8536d893a720769074c272b23bf63
DanielKelley117 <117874058+DanielKelley117@users.noreply.github.com>
2022-12-21 20:34:08 -0500

    Update AircraftCarrier.java

---
+++ AircraftCarrier.java
@@ -0,0 +1,11 @@
+//My name is Daniel Kelley and this is part of my final project for CS110. This contains the data for an Aircraft Carrier object, which is a Ship.
+public class AircraftCarrier extends Ship
+{
+   /*This is the AircraftCarrier constructor.
+      It takes in no arguments, and just calls the super constructor with a size of 5.
+   */
+   public AircraftCarrier()
+   {
+      super(5);
+   }
+}
