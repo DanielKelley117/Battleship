a4537c74380a1c4178dadfa62acf446ec8f259e9
DanielKelley117 <117874058+DanielKelley117@users.noreply.github.com>
2022-12-21 20:40:45 -0500

    Update Sub.java

---
+++ Sub.java
@@ -0,0 +1,11 @@
+//My name is Daniel Kelley and this is part of my CS110 final project. This class contains the data for a Sub object, which is a ship.
+public class Sub extends Ship
+{
+   /*This is the Sub constructor. 
+      This constructor takes in no arguments, and just calls the superconstructor with an argument of 3.
+   */
+   public Sub()
+   {
+      super(3);
+   }
+}
