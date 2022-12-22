970e81e8a203ed0d04cef313f34d042ede1da170
DanielKelley117 <117874058+DanielKelley117@users.noreply.github.com>
2022-12-21 20:37:18 -0500

    Update Cruiser.java

---
+++ Cruiser.java
@@ -0,0 +1,11 @@
+//My name is Daniel Kelley and this is part of my CS110 final project. This class has the data for a Cruiser object, which is a ship.
+public class Cruiser extends Ship
+{
+   /*This is the Cruiser constructor
+      It takes in no arguments, and just calls the super constructor with an argument of 3.
+   */
+   public Cruiser()
+   {
+      super(3);
+   }
+}
